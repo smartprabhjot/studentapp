be09ca084d77d79b21f31f1ee975086f6acddaf2
Prabhjot.Kaur3 <prabhjot.kaur3@student.ufv.ca>
2020-12-07 16:31:35 -0800

    Different classes added

--- app/src/main/java/com/example/studentapp/MainActivity.kt
+++ app/src/main/java/com/example/studentapp/MainActivity.kt
@@ -2,13 +2,33 @@ package com.example.studentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import java.util.jar.Attributes
 
 class MainActivity : AppCompatActivity() {
+
+    private EditText name;
+    private EditText password;
+    private Button signUp;
+    private Button login;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
 
+        name = findViewByID(R.id.editEmailAddress);
+        password = findViewByID(R.id.editPassword);
+        signUp=findViewByID(R.id.signUpButton);
+        signIn=findViewByID(R.id.signInButton);
+
+
+
+
 
 
     }
+
+
+
 }
