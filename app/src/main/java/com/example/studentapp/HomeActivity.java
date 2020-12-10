36969ee4372dd9db0be2ad0556425894b5a00844
Prabhjot.Kaur3 <prabhjot.kaur3@student.ufv.ca>
2020-12-09 23:01:23 -0800

    Adding new views

--- app/src/main/java/com/example/studentapp/HomeActivity.java
+++ app/src/main/java/com/example/studentapp/HomeActivity.java
@@ -1,14 +1,31 @@
 package com.example.studentapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeActivity extends AppCompatActivity {
+  private Button logout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(HomeActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/studentapp/MainActivity.java
+++ app/src/main/java/com/example/studentapp/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.studentapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText email;
-    EditText password;
-    Button signIn;
-    Button signUp;
+    private EditText email;
+    private EditText password;
+    private  Button signIn;
+    private Button signUp;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.editRegPassword);
         signIn = (Button) findViewById(R.id.signInButton);
         signUp = (Button) findViewById(R.id.signUpButton);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,12 +49,51 @@ public class MainActivity extends AppCompatActivity {
 
         signIn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
+               // Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                //startActivity(homeIntent);
+                Login();
             }
         });
 
 
+
+    }
+
+    private void Login(){
+        String newEmail = email.getText().toString();
+        String newPassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(newEmail)){
+            email.setError("Please enter your email");
+            return;
+        }
+
+        else if(TextUtils.isEmpty(newPassword)){
+            password.setError("Please enter your password");
+            return;
+        }
+
+
+        progressDialog.setMessage("Please wait..");
+        progressDialog.show();
+        progressDialog.setCanceledOnTouchOutside(false);
+        firebaseAuth.signInWithEmailAndPassword(newEmail,newPassword).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this,"Sign in is sucessful!",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"Failure!"+task.getException(),Toast.LENGTH_LONG).show();
+                }
+
+                progressDialog.dismiss();
+            }
+        });
 
     }
 }
